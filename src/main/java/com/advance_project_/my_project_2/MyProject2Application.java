33aa2554b0c69a81c6a790afed66311bd50e5cbc
nataliMircheva <nataliadimova@abv.bg>
2021-12-01 18:39:09 +0200

    initial commit

---
+++ src/main/java/com/advance_project_/my_project_2/MyProject2Application.java
@@ -0,0 +1,13 @@
+package com.advance_project_.my_project_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/advance_project_/my_project_2/MyProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advance_project_.my_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
